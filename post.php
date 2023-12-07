059e44ba71ab6b009de99782b0d0948b1bc6a196
fajlay-rabbi <sheikhfajlayrabbi@gmail.com>
2023-12-07 19:21:46 +0600

    Adding more feature to post page

--- post.php
+++ post.php
@@ -1,4 +1,29 @@
+<?php
 
+    require_once "./includes/Narvbar.php";
+    require_once "./includes/Footer.php";
+
+
+
+    session_start();
+
+    if(!isset($_SESSION['name'])){
+        header("Location: signin.php");
+        exit();
+    }
+
+    $ISLOGIN = true;
+    $img = $_SESSION['image'];
+
+
+    // include_once "config.php";
+    // $user = $_SESSION['user'];
+    // $sql = mysqli_query($conn, "SELECT * FROM users WHERE id = {$user}");
+    // if(mysqli_num_rows($sql) > 0){
+    //     $row = mysqli_fetch_assoc($sql);
+    // }
+
+?>
 
 
 <!DOCTYPE html>
@@ -10,16 +35,26 @@
     <link rel="stylesheet" href="./src/assets/fonts/AlinurTumatulFont/stylesheet.css">
     <link rel="stylesheet" href="./src/assets/fonts/sirajiFont/stylesheet.css">
     <link rel="stylesheet" href="./src/css/poststyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <title>পোস্ট করুন</title>
 </head>
 <body>
     <div class="container">
+        <?php Navbar($ISLOGIN, $img) ?>
         <div class="wrapper">
             <p>পোস্ট করুন</p>
 
         </div>
     </div>
+
+
+
+
+
+
+    <?php myFooter(); ?>
+    <script type="text/javascript" src="./src/js/script.js"></script>
 </body>
 </html>
