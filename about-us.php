b0b203fc4b7108ad131cffb9964b2c87b3fba83e
fajlay-rabbi <sheikhfajlayrabbi@gmail.com>
2023-12-09 21:30:45 +0600

    Adding about us page

---
+++ about-us.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        আমাদের সম্পর্কে
+    </title>
+</head>
+<body>
+    <h1>
+        আমাদের সম্পর্কে 
+    </h1>
+</body>
+</html>
